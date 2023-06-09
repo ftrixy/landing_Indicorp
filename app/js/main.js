5acc8c50c38b0d953498d5153b7a0a088457a3ea
ftrixy <dmitry.kravetss@gmail.com>
2023-06-09 14:48:56 +0300

    fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 $(function () {
 
@@ -25,21 +25,7 @@ $(function () {
                         // showinfo: 0
                 },
         });
-=======
-$(function(){
 
-    $(".menu a, .header a, .footer a").on("click", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 1500);
-    });
-
-    $('.header__burger').on('click', function(){
-        $('.menu__list').toggleClass('menu__list--active');
-        $('.header__burger').toggleClass('header__burger--active');
-    });
->>>>>>> 80877145a0d9d1d686c82bc04e13008702c15cb7
 });
 
 
@@ -47,18 +33,16 @@ $(function(){
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener("click", function (event) {
-    event.preventDefault();
+        anchor.addEventListener("click", function (event) {
+                event.preventDefault();
 
-    const blockID = anchor.getAttribute('href')
+                const blockID = anchor.getAttribute('href')
 
-    document.querySelector('' + blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    })
-  })
-<<<<<<< HEAD
+                document.querySelector('' + blockID).scrollIntoView({
+                        behavior: "smooth",
+                        block: "start"
+                })
+        })
+
 }
-=======
-}
->>>>>>> 80877145a0d9d1d686c82bc04e13008702c15cb7
+
